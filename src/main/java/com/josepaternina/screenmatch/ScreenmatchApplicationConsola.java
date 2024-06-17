7f90ce8ce975493920fabd8f3ffd81ed21010a77
PaterDev <josepater_1130@hotmail.com>
2024-06-16 20:29:33 -0500

    feat: using Spring Web

---
+++ src/main/java/com/josepaternina/screenmatch/ScreenmatchApplicationConsola.java
@@ -0,0 +1,41 @@
+/*
+package com.josepaternina.screenmatch;
+
+import com.josepaternina.screenmatch.principal.Principal;
+import com.josepaternina.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplicationConsola implements CommandLineRunner {
+
+	@Autowired // Inyección de dependencia
+	private SerieRepository repository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplicationConsola.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal(repository);
+
+		try {
+			principal.mostrarMenu();
+		} catch (Exception e) {
+			System.out.println("--- Pequeño errror: ---\n" + e.getMessage());
+		}
+
+		*/
+/*
+		EjemploStreams ejemploStreams= new EjemploStreams();
+		ejemploStreams.muestraEjemplo();
+		*//*
+
+
+	}
+
+}
+*/
